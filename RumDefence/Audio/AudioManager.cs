270bced4bf196ceea1e9db6bb3b650aab2b7cefd
jimtronchet <jim@web-mine.nl>
2026-05-03 17:47:06 +0200

    Add shooting audio for towers

--- RumDefence/Audio/AudioManager.cs
+++ RumDefence/Audio/AudioManager.cs
@@ -33,6 +33,7 @@ public class AudioManager
         soundEffects["confirmation"] = content.Load<SoundEffect>("Audio/confirmation_002");
         soundEffects["error"] = content.Load<SoundEffect>("Audio/error_008");
         soundEffects["switch"] = content.Load<SoundEffect>("Audio/switch_002");
+        soundEffects["shoot"] = content.Load<SoundEffect>("Audio/shoot");
 
         for (int i = 0; i < 5; i++)
             footstepSounds.Add(content.Load<SoundEffect>($"Audio/footstep_grass_00{i}"));
