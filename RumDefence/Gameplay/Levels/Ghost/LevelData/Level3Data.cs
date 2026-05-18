b083fec2db01410e525a31d6f3100ecf91d02a35
Tim Grootscholten <55889515+TimGrootscholten@users.noreply.github.com>
2026-05-18 13:22:56 +0200

    Add ghost troop that phases through walls

--- RumDefence/Gameplay/Levels/Ghost/LevelData/Level3Data.cs
+++ RumDefence/Gameplay/Levels/Ghost/LevelData/Level3Data.cs
@@ -35,16 +35,17 @@ public static class Level3Data
 
     private static readonly TroopData Regular = TroopFactory.Regular;
     private static readonly TroopData Boss = TroopFactory.Boss;
+    private static readonly TroopData Ghost = TroopFactory.Ghost;
 
     private static List<Wave> Waves => new()
     {
         //                                                                    ships  troops                                spawnDelay
         CreateWave(minSpawnTime: 3f,   maxSpawnTime: 6f,   holdingTime: 5f, (NormalShip,  1, [(Regular,  5, 100)],              1.0f)),
         CreateWave(minSpawnTime: 2f,   maxSpawnTime: 4f,   holdingTime: 0f, (NormalShip,  2, [(Regular,  7, 100)],              1.0f)),
-        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 9f,   holdingTime: 0f, (NormalShip,  3, [(Regular,  6, 100)],1.5f), (BossShip, 1, [(Boss, 1, 500)], 0.5f)),
-        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 7f,   holdingTime: 0f, (NormalShip,  3, [(Regular, 8, 100)],0.8f), (BossShip, 1, [(Boss, 1, 500)], 0.5f)),
-        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 4f,   holdingTime: 0f, (NormalShip,  20, [(Regular, 4, 100)], 0.5f), (BossShip, 2, [(Boss, 1, 600)], 0.5f)),
-        CreateWave(minSpawnTime: 0.5f, maxSpawnTime: 8f, holdingTime: 0f, (NormalShip, 10, [(Regular, 10, 100)], 0.3f), (BossShip, 3, [(Boss, 1, 600)], 0.3f)),
+        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 9f,   holdingTime: 0f, (NormalShip,  3, [(Regular,  6, 100), (Ghost, 2, 50)],1.5f), (BossShip, 1, [(Boss, 1, 500)], 0.5f)),
+        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 7f,   holdingTime: 0f, (NormalShip,  3, [(Regular, 8, 100), (Ghost, 3, 50)],0.8f), (BossShip, 1, [(Boss, 1, 500)], 0.5f)),
+        CreateWave(minSpawnTime: 1f,   maxSpawnTime: 4f,   holdingTime: 0f, (NormalShip,  20, [(Regular, 4, 100), (Ghost, 2, 50)], 0.5f), (BossShip, 2, [(Boss, 1, 600)], 0.5f)),
+        CreateWave(minSpawnTime: 0.5f, maxSpawnTime: 8f, holdingTime: 0f, (NormalShip, 10, [(Regular, 10, 100), (Ghost, 3, 50)], 0.3f), (BossShip, 3, [(Boss, 1, 600)], 0.3f)),
     };
 
     private static string[] MapData => new[]
