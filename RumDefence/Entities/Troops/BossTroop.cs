84cffe3d293ea3d6c9a561816f4f0df5182a89fd
Tobias Roessingh <tobias.roessingh@gmail.com>
2026-05-24 21:26:33 +0200

    Remove unused animations and pathfinding logic for Bomber and Boss troops

--- RumDefence/Entities/Troops/BossTroop.cs
+++ RumDefence/Entities/Troops/BossTroop.cs
@@ -7,7 +7,5 @@ public class BossTroop : Troop
     public BossTroop(TroopData data, Vector2 start, Vector2 target)
         : base(data, start, target)
     {
-        // _walkanimation = new BossTroopAnimation(16, 16, 0.2f, true);
-        // _swordAttackAnimation = new BossTroopSwordAttackAnimation();
     }
 }
