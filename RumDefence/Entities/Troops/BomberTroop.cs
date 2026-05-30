8ba1628e32332abdca7dde24f5d83e632f36b3eb
Tobias Roessingh <tobias.roessingh@gmail.com>
2026-05-30 17:27:48 +0200

    bomber pathfinding only avoids water and walks in straight to the barrel

--- RumDefence/Entities/Troops/BomberTroop.cs
+++ RumDefence/Entities/Troops/BomberTroop.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace RumDefence;
 
@@ -27,6 +28,20 @@ public class BomberTroop : Troop
     }
 
     protected override bool CanAttackWalls => false;
+
+    public override void UpdatePathfinding()
+    {
+        NeedsPathInit = false;
+        var grid = RumGame.Instance.CurrentGrid;
+
+        var waterOnly = new HashSet<Point>();
+        for (int x = 0; x < grid.Width; x++)
+            for (int y = 0; y < grid.Height; y++)
+                if (TileRules.IsWater(grid.Tiles[y, x]))
+                    waterOnly.Add(new Point(x, y));
+
+        pathfinding.UpdatePath(Position, grid, waterOnly);
+    }
 
     public override void Update(GameTime gameTime)
     {
