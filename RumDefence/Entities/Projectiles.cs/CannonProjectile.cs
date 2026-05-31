d21d37ab1713f081ed8ccd03fd0cb66cf97296f1
Tobias Roessingh <tobias.roessingh@gmail.com>
2026-05-31 23:14:45 +0200

    Squashed commit of the following:
    commit 37cd2117aa249102346c74662992ad7cd08c9a0c
    Author: Tobias Roessingh <tobias.roessingh@gmail.com>
    Date:   Sun May 31 22:59:19 2026 +0200
    
        update bullet and cannonball sprites and projectile animation
    
    commit b9839b68b32827c51148e48ed07c351a35b1caeb
    Author: Tobias Roessingh <tobias.roessingh@gmail.com>
    Date:   Sun May 31 22:03:59 2026 +0200
    
        Merge commit 'bfc08c6b99e2bd7b3ce8f7abbbcc0ced17c8cca7'

--- RumDefence/Entities/Projectiles.cs/CannonProjectile.cs
+++ RumDefence/Entities/Projectiles.cs/CannonProjectile.cs
@@ -9,6 +9,7 @@ public class CannonProjectile : BaseProjectile
 {
     private readonly float _aoeRadius;
     private readonly Tuple<float, float> _aoeDamageRange = new(0.2f, 0.8f);
+    private readonly Animation _animation;
 
     public CannonProjectile(Vector2 start, Troop target, float speed, int damage, float aoeRadius)
         : base(start, target, speed, damage)
@@ -16,13 +17,33 @@ public class CannonProjectile : BaseProjectile
         ApplyDirectDamage = false;
         _aoeRadius = aoeRadius;
         Texture = RumGame.Instance.Content.Load<Texture2D>("Art/Projectiles/cannonball");
-        Size = SizeSystem.Square(0.25f);
+
+        int frameWidth = Texture.Width / 4;
+        int frameHeight = Texture.Height;
+        Size = SizeSystem.Square(0.7f);
+        origin = new Vector2(frameWidth / 2f, frameHeight / 2f);
+        scale = SizeSystem.ToScale(frameWidth, Size);
+
+        Vector2 dir = target.Position - start;
+        rotation = MathF.Atan2(dir.Y, dir.X) - MathF.PI;
+
+        _animation = new Animation(frameWidth, frameHeight, 0.1f);
+        _animation.AddLayerMatrix([new(4, SpriteAction.Static, SpriteDirection.None)]);
+        _animation.ActivateLayers([new(SpriteAction.Static, SpriteDirection.None)]);
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        var layers = _animation.GetCurrentLayers();
+        foreach (var layer in layers)
+            spriteBatch.Draw(Texture, Position, layer.Item2, color, rotation + rotationOffset, origin, scale, layer.Item1.Effect, layer.Item1.Depth);
     }
 
     public override void Update(GameTime gameTime)
     {
         if (IsFinished) return;
 
+        _animation.Update(gameTime);
         base.Update(gameTime);
 
         if (IsFinished)
