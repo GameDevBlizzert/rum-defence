f48b01b94de058d0f241d377d31372ba7bf5eff3
tobitotaku <troessingh@bitbucket.org>
2026-06-05 16:46:26 +0200

    move BoxItem to IBox

--- RumDefence/UI/Box/IBox.cs
+++ RumDefence/UI/Box/IBox.cs
@@ -8,6 +8,19 @@ namespace RumDefence.UI.Box;
 public enum Align { Start, Center, Between, End }
 public enum Direction { Row, Column }
 
+
+public interface IBoxItem
+{
+    Align AlignX { get; set; }
+    Align AlignY { get; set; }
+    int Span { get; set; }
+    Rectangle Slot { get; set; }
+    Vector2 Measure();
+    void Update(GameTime gameTime);
+    void Arrange(Rectangle rect);
+    void Draw(SpriteBatch spriteBatch);
+}
+
 public interface IBox : IBoxItem
 {
     int Columns { get; set; }
@@ -17,4 +30,24 @@ public interface IBox : IBoxItem
     int Width { get; set; }
     int Height { get; set; }
 
+}
+
+public abstract class BoxItem : IBoxItem
+{
+    public Align AlignX { get; set; } = Align.Center;
+    public Align AlignY { get; set; } = Align.Center;
+    public int Span { get; set; } = 1;
+    public Color Color { get; set; } = Color.White;
+    public Rectangle Slot { get; set; }
+    // Measure the contents
+    public virtual Vector2 Measure()
+    {
+        return Vector2.Zero;
+    }
+    public virtual void Arrange(Rectangle rect) => Slot = rect;
+    public virtual void Update(GameTime gameTime) { }
+    public virtual void Draw(SpriteBatch spriteBatch)
+    {
+
+    }
 }
