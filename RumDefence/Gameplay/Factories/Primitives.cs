0dcc03680e8312b93e6fb8a21eeb69a9f56cfe6b
tobitotaku <troessingh@bitbucket.org>
2026-06-11 17:52:56 +0200

    fix inverted texture

--- RumDefence/Gameplay/Factories/Primitives.cs
+++ RumDefence/Gameplay/Factories/Primitives.cs
@@ -39,7 +39,7 @@ public static class Primitives
     private static Texture2D _buttonTexture;
     public static Texture2D ButtonTexture => _buttonTexture ??= RumGame.Instance.Content.Load<Texture2D>("Art/UI/Buttons/button");
     private static Texture2D _buttonInvertedTexture;
-    public static Texture2D ButtonInvertedTexture => _buttonInvertedTexture ??= RumGame.Instance.Content.Load<Texture2D>("Art/UI/buttonInverteds/button.inverted");
+    public static Texture2D ButtonInvertedTexture => _buttonInvertedTexture ??= RumGame.Instance.Content.Load<Texture2D>("Art/UI/Buttons/button.inverted");
     public static Color FontColor => new Color(255, 200, 0);
     public static Color FontDarkColor => new Color(215, 90, 0);
     public static Color FontLightColor => new Color(255, 225, 120);
