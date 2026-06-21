d6a28e7cea80bdb777a8fe3dee793f3d71c0bb62
Tim Grootscholten <55889515+TimGrootscholten@users.noreply.github.com>
2026-06-21 17:33:36 +0200

    easy (#109)

--- RumDefence/Gameplay/Levels/Grass/LevelData/Level1Data.cs
+++ RumDefence/Gameplay/Levels/Grass/LevelData/Level1Data.cs
@@ -43,10 +43,10 @@ public static class Level1Data
     private static List<Wave> Waves => new()
     {
         //                                                                   ships  troops                                              spawnDelay
-        CreateWave(minSpawnTime: 10f, maxSpawnTime: 20f, (NormalShip,  1, [(Regular,   10,  50)],                            2f), (NormalShip, 1, [(Regular, 10, 80)], 2f)),
-        CreateWave(minSpawnTime:  7f, maxSpawnTime: 13f, (NormalShip,  2, [(Regular,   4,  100)],                           1.0f), (NormalShip, 1, [(Bomber, 2, 80)], 3f)),
-        CreateWave(minSpawnTime:  0f, maxSpawnTime:  3f, (NormalShip,  2, [(Regular,   7,  100)],                           0.6f), (NormalShip, 1, [(Bomber, 2, 80)], 3f)),
-        CreateWave(minSpawnTime:  6f, maxSpawnTime: 11f, (NormalShip,  6, [(Regular,   4,  100)],                             1f), (BossShip, 1, [(Boss, 2, 500)], 0.1f), (NormalShip, 1, [(Bomber, 3, 80)], 2f)),
+        CreateWave(minSpawnTime: 10f, maxSpawnTime: 20f, (NormalShip,  1, [(Regular,   10,  25)],                            2f), (NormalShip, 1, [(Regular, 10, 50)], 2f)),
+        CreateWave(minSpawnTime:  7f, maxSpawnTime: 13f, (NormalShip,  2, [(Regular,   4,  50)],                           1.0f), (NormalShip, 1, [(Bomber, 2, 50)], 3f)),
+        CreateWave(minSpawnTime:  0f, maxSpawnTime:  3f, (NormalShip,  2, [(Regular,   7,  50)],                           0.6f), (NormalShip, 1, [(Regular, 2, 50)], 3f)),
+        CreateWave(minSpawnTime:  6f, maxSpawnTime: 11f, (NormalShip,  6, [(Regular,   4,  50)],                             1f), (BossShip, 1, [(Boss, 1, 500)], 0.1f), (NormalShip, 1, [(Bomber, 3, 80)], 2f)),
     };
 
     private static string[] MapData => StandardMap.Layout;
